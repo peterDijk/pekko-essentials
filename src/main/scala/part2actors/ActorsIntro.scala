52f30958a7f34e07b06bd9383a02d2646bf256dc
Peter van Dijk <peter.van.dijk@bynder.com>
2025-07-11 16:00:41 +0200

    practical intro to actors

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -5,9 +5,9 @@ import org.apache.pekko.actor.typed.scaladsl.Behaviors
 
 object ActorsIntro {
 
-  // part 1: behavior
-  val simpleActorBehavior: Behavior[String] = Behaviors.receiveMessage { (message: String) =>
-    // do something with the message
+  // part 1
+  // behaviour
+  val simpleActorBehavior: Behavior[String] = Behaviors.receiveMessage{ (message: String) =>
     println(s"[simple actor] I have received: $message")
 
     // new behavior for the NEXT message
@@ -15,22 +15,20 @@ object ActorsIntro {
   }
 
   def demoSimpleActor(): Unit = {
-    // part 2: instantiate
-    val actorSystem = ActorSystem(Person.happy(), "FirstActorSystem")
+    // part 2: Instantiate
+    val actorSystem = ActorSystem[String](SimpleActor_v2(), "FirstActorSystem")
 
-    // part 3: communicate!
-    actorSystem ! "I am learning Pekko" // asynchronously send a message
-    // ! = the "tell" method
+    // part 3: Communicate
+    actorSystem ! "I am learning Pekko"
+//    actorSystem.tell("I am learning Pekko")
 
     // part 4: gracefully shut down
     Thread.sleep(1000)
     actorSystem.terminate()
   }
 
-  // "refactor"
   object SimpleActor {
-    def apply(): Behavior[String] = Behaviors.receiveMessage { (message: String) =>
-      // do something with the message
+    def apply(): Behavior[String] = Behaviors.receiveMessage{ (message: String) =>
       println(s"[simple actor] I have received: $message")
 
       // new behavior for the NEXT message
@@ -38,21 +36,20 @@ object ActorsIntro {
     }
   }
 
-  object SimpleActor_V2 {
+  object SimpleActor_v2 {
     def apply(): Behavior[String] = Behaviors.receive { (context, message) =>
-      // context is a data structure (ActorContext) with access to a variety of APIs
+      // context is a data structure that has access to a lot of APIs
       // simple example: logging
       context.log.info(s"[simple actor] I have received: $message")
       Behaviors.same
     }
   }
 
-  object SimpleActor_V3 {
-    def apply(): Behavior[String] = Behaviors.setup { context =>
-      // actor "private" data and methods, behaviors etc
-      // YOUR CODE HERE
+  object SimpleActor_v3 {
+    def apply(): Behavior[String] = Behaviors.setup { (context) =>
+      // for actor "private" data and behaviour
+      // YOUR CODE WHATEVER
 
-      // behavior used for the FIRST message
       Behaviors.receiveMessage { message =>
         context.log.info(s"[simple actor] I have received: $message")
         Behaviors.same
@@ -60,104 +57,7 @@ object ActorsIntro {
     }
   }
 
-  /**
-   * Exercises
-   * 1. Define two "person" actor behaviors, which receive Strings:
-   *  - "happy", which logs your message, e.g. "I've received ____. That's great!"
-   *  - "sad", .... "That sucks."
-   *  Test both.
-   *
-   * 2. Change the actor behavior:
-   *  - the happy behavior will turn to sad() if it receives "Pekko is bad."
-   *  - the sad behavior will turn to happy() if it receives "Pekko is awesome!"
-   *
-   * 3. Inspect my code and try to make it better.
-   */
-
-  object Person {
-    def happy(): Behavior[String] = Behaviors.receive { (context, message) =>
-      message match {
-        case "Pekko is bad." =>
-          context.log.info("Don't you say anything bad about Pekko!!!")
-          sad()
-        case _ =>
-          context.log.info(s"I've received '$message'.That's great!")
-          Behaviors.same
-      }
-    }
-
-    def sad(): Behavior[String] = Behaviors.receive { (context, message) =>
-      message match {
-        case "Pekko is awesome!" =>
-          context.log.info("Happy now!")
-          happy()
-        case _ =>
-          context.log.info(s"I've received '$message'.That sucks!")
-          Behaviors.same
-      }
-    }
-
-    def apply(): Behavior[String] = happy()
-  }
-
-  def testPerson(): Unit = {
-    val person = ActorSystem(Person(), "PersonTest")
-
-    person ! "I love the color blue."
-    person ! "Pekko is bad."
-    person ! "I also love the color red."
-    person ! "Pekko is awesome!"
-    person ! "I love Pekko."
-
-    Thread.sleep(1000)
-    person.terminate()
-  }
-
-  object WeirdActor {
-    // wants to receive messages of type Int AND String
-    def apply(): Behavior[Any] = Behaviors.receive { (context, message) =>
-      message match {
-        case number: Int =>
-          context.log.info(s"I've received an int: $number")
-          Behaviors.same
-        case string: String =>
-          context.log.info(s"I've received a String: $string")
-          Behaviors.same
-      }
-    }
-  }
-
-  // solution: add wrapper types & type hierarchy (case classes/objects)
-  object BetterActor {
-    trait Message
-    case class IntMessage(number: Int) extends Message
-    case class StringMessage(string: String) extends Message
-
-    def apply(): Behavior[Message] = Behaviors.receive { (context, message) =>
-      message match {
-        case IntMessage(number) =>
-          context.log.info(s"I've received an int: $number")
-          Behaviors.same
-        case StringMessage(string) =>
-          context.log.info(s"I've received a String: $string")
-          Behaviors.same
-      }
-    }
-  }
-
-
-  def demoWeirdActor(): Unit = {
-    import BetterActor._
-    val weirdActor = ActorSystem(BetterActor(), "WeirdActorDemo")
-    weirdActor ! IntMessage(43) // ok
-    weirdActor ! StringMessage("Pekko") // ok
-    // weirdActor ! '\t' // not ok
-
-    Thread.sleep(1000)
-    weirdActor.terminate()
-  }
-
   def main(args: Array[String]): Unit = {
-    demoWeirdActor()
+    demoSimpleActor()
   }
 }
