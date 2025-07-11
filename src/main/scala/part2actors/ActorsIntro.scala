a64f1cf168c53f7256216675de1240f41aef148d
Peter van Dijk <peter.van.dijk@bynder.com>
2025-07-11 18:48:16 +0200

    actor intro

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -16,11 +16,10 @@ object ActorsIntro {
 
   def demoSimpleActor(): Unit = {
     // part 2: Instantiate
-    val actorSystem = ActorSystem[String](SimpleActor_v2(), "FirstActorSystem")
+    val actorSystem = ActorSystem[String](SimpleActor_v3(), "FirstActorSystem")
 
     // part 3: Communicate
-    actorSystem ! "I am learning Pekko"
-//    actorSystem.tell("I am learning Pekko")
+    actorSystem ! "Pekko is bad"
 
     // part 4: gracefully shut down
     Thread.sleep(1000)
@@ -57,7 +56,84 @@ object ActorsIntro {
     }
   }
 
+  object Person {
+    def happy(): Behavior[String] = Behaviors.receive { (context, message) =>
+      message match {
+        case "Pekko is bad" =>
+          context.log.info("dont say bad on pekko")
+          sad()
+        case _ =>
+          context.log.info(s"I've received --$message. That's great!")
+          Behaviors.same
+      }
+    }
+    def sad(): Behavior[String] = Behaviors.receive { (context, message) =>
+      message match {
+        case "Pekko is awesome" =>
+          context.log.info("knew you'd like it")
+          happy()
+        case _ =>
+          context.log.info(s"I've received --$message. That sucks")
+          Behaviors.same
+      }
+    }
+
+    def apply() = happy()
+  }
+
+  object WeirdActor {
+    def apply(): Behavior[Any] = Behaviors.receive{ (context, message) =>
+      message match {
+        case aInt: Int =>
+          context.log.info(s"I received an Int: $aInt")
+          Behaviors.same
+        case sString: String =>
+          context.log.info(s"I received a String: $sString")
+          Behaviors.same
+      }
+    }
+  }
+
+  object BetterActor {
+    trait Message
+    case class IntMessage(number: Int) extends Message
+    case class StringMessage(aString: String) extends Message
+
+    def apply(): Behavior[Message] = Behaviors.receive { (context, message) =>
+      message match {
+        case IntMessage(aNumber) =>
+          context.log.info(s"I received an Int: $aNumber")
+          Behaviors.same
+        case StringMessage(aString) =>
+          context.log.info(s"I received a String: $aString")
+          Behaviors.same
+      }
+    }
+  }
+
+  def testPerson(): Unit =
+    val person = ActorSystem(Person(), "PersonTest")
+
+    person ! "I love blue"
+    person ! "Pekko is bad"
+    person ! "I also love red"
+    person ! "Pekko is awesome"
+    person ! "Pekko is ok"
+
+    Thread.sleep(1000)
+    person.terminate()
+
+  def demoBetterActor(): Unit = {
+    import BetterActor._
+    val actor = ActorSystem(BetterActor(), "ActorTest")
+
+    actor ! IntMessage(43)
+    actor ! StringMessage("Hoooiiiii")
+  }
+
   def main(args: Array[String]): Unit = {
-    demoSimpleActor()
+//    demoSimpleActor()
+//    testPerson()
+    demoBetterActor()
   }
 }
