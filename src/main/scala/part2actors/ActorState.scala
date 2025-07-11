3d12156e5642faf2e4295382e7626ef5ce2881f4
Peter van Dijk <peter.van.dijk@bynder.com>
2025-07-11 20:01:26 +0200

    not adding up?\

--- src/main/scala/part2actors/ActorState.scala
+++ src/main/scala/part2actors/ActorState.scala
@@ -1,7 +1,7 @@
 package part2actors
 
 import org.apache.pekko.actor.typed.{ActorSystem, Behavior}
-import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.scaladsl.{Behaviors}
 
 object ActorState {
 
@@ -14,12 +14,12 @@ object ActorState {
 
    object WordCounter {
      def apply(): Behavior[String] = Behaviors.setup { context =>
-     var totalWords: Int = 0
+     var totalWords = 0
 
        Behaviors.receiveMessage { message =>
          val splitWords = message.split(" ")
          val countWords: Int = splitWords.length
-         totalWords += countWords
+         totalWords += 1
          context.log.info(s"The current number of words is: $countWords")
          context.log.info(s"The total number of words is now: $totalWords")
          Behaviors.same
@@ -38,9 +38,51 @@ object ActorState {
      actorSystem.terminate()
    }
 
+  trait SimpleThing
+  case object EatChocolate extends SimpleThing
+  case object CleanUpTheFloor extends SimpleThing
+  case object LearnPekko extends SimpleThing
+  /*
+    Message types must be IMMUTABLE and SERIALIZABLE.
+    - use case classes/objects
+    - use a flat type hierarchy
+   */
+
+  object SimpleHuman {
+    def apply(): Behavior[SimpleThing] = Behaviors.setup { context =>
+      var happiness = 0
+
+      Behaviors.receiveMessage {
+        case EatChocolate =>
+          context.log.info(s"[$happiness] Eating chocolate")
+          happiness += 1
+          Behaviors.same
+        case CleanUpTheFloor =>
+          context.log.info(s"[$happiness] Wiping the floor, ugh...")
+          happiness -= 2
+          Behaviors.same
+        case LearnPekko =>
+          context.log.info(s"[$happiness] Learning Pekko, YAY!")
+          happiness += 99
+          Behaviors.same
+      }
+    }
+  }
+
+  def demoSimpleHuman(): Unit = {
+    val human = ActorSystem(SimpleHuman(), "DemoSimpleHuman")
+
+    human ! LearnPekko
+    human ! EatChocolate
+    (1 to 30).foreach(_ => human ! CleanUpTheFloor)
+
+    Thread.sleep(1000)
+    human.terminate()
+  }
+
   def main(args: Array[String]): Unit = {
     wordActorSystem()
-
+//  demoSimpleHuman()
 
   }
 }
