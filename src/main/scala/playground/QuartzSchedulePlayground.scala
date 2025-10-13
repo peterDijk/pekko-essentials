8a433ba3256e43115afd587232a6b16d3a39d0c1
Peter van Dijk <peter.van.dijk@bynder.com>
2025-10-13 09:43:35 +0200

    update with resume logic

--- src/main/scala/playground/QuartzSchedulePlayground.scala
+++ src/main/scala/playground/QuartzSchedulePlayground.scala
@@ -1,7 +1,7 @@
 package playground
 
 import org.apache.pekko.actor.typed.{ActorRef, ActorSystem, Behavior}
-import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.scaladsl.{ActorContext, Behaviors}
 import org.apache.pekko.extension.quartz.QuartzSchedulerTypedExtension
 
 trait ScheduledMessages
@@ -12,30 +12,41 @@ case object Stop extends ScheduledMessages
 object QuartzSchedulePlayground {
 
   object ScheduledActor {
-    def apply(scheduleCronExpression: String): Behavior[ScheduledMessages] = created(scheduleCronExpression: String)
-    private def created(scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.setup { (context ) =>
-      context.log.info(s"Actor created, starting scheduling...")
-
-      // schedule messages with quartz
+    def startScheduler(scheduleName: String, scheduleCronExpression: String, context: ActorContext[ScheduledMessages]): Unit = {
       val quartzScheduler = QuartzSchedulerTypedExtension(context.system)
       quartzScheduler.createTypedJobSchedule[ScheduledMessages](
-        name = "myJobName_1",
+        name = scheduleName,
         receiver = context.self,
         msg = Tick,
         cronExpression = scheduleCronExpression)
-
-      active(scheduleCronExpression)
     }
 
-    def active(scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.receive[ScheduledMessages] { (context, message) =>
+    def apply(scheduleName: String, scheduleCronExpression: String): Behavior[ScheduledMessages] = created(scheduleName, scheduleCronExpression)
+    private def created(scheduleName: String, scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.setup { (context ) =>
+      context.log.info(s"Actor created, starting scheduling...")
+
+      // schedule messages with quartz
+      startScheduler(
+        scheduleName,
+        scheduleCronExpression,
+        context
+      )
+
+      active(scheduleName, scheduleCronExpression)
+    }
+
+    def active(scheduleName: String, scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.receive[ScheduledMessages] { (context, message) =>
       message match {
         case Tick =>
           context.log.info("Received a Tick...")
           // Can call whatever outside services here
           Behaviors.same
         case Stop =>
-          context.log.info("Stopping...")
-          stopped(scheduleCronExpression)
+          context.log.info("Stopping scheduler...")
+          val quartzScheduler = QuartzSchedulerTypedExtension(context.system)
+          quartzScheduler.cancelJob(scheduleName)
+          context.log.info("Scheduler stopped")
+          stopped(scheduleName, scheduleCronExpression)
         case _ =>
           context.log.info("Unknown message")
           Behaviors.same
@@ -46,28 +57,25 @@ object QuartzSchedulePlayground {
         Behaviors.same
     }
 
-    def stopped(scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.setup { context =>
-      context.log.info("Stopping scheduler...")
-      val quartzScheduler = QuartzSchedulerTypedExtension(context.system)
-      quartzScheduler.cancelJob(scheduleCronExpression)
-      context.log.info("Scheduler stopped")
-      Behaviors.same
-    }
+    def stopped(scheduleName: String, scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.receive[ScheduledMessages] { (context, message) =>
+      message match {
+        case Start =>
+          // schedule messages with quartz
+          startScheduler(
+            scheduleName,
+            scheduleCronExpression,
+            context
+          )
 
-    def restarted(scheduleCronExpression: String): Behavior[ScheduledMessages] = Behaviors.setup { context =>
-      context.log.info("Restarting scheduler...")
-      val quartzScheduler = QuartzSchedulerTypedExtension(context.system)
-      quartzScheduler.scheduleTyped(scheduleCronExpression, context.self, Tick)
-      context.log.info("Scheduler restarted")
-      active(scheduleCronExpression)
+          active(scheduleName, scheduleCronExpression)
+      }
     }
-
   }
 
   def demoQuartzScheduling(): Unit = {
     val guardian: Behavior[Unit] = Behaviors.setup { context =>
       val scheduleCronExpression: String = "*/10 * * ? * *" // Will fire every ten seconds
-      val scheduledActor = context.spawn(ScheduledActor(scheduleCronExpression), "scheduledActor")
+      val scheduledActor = context.spawn(ScheduledActor("jobName_1", scheduleCronExpression), "scheduledActor")
 
       Behaviors.empty
     }
